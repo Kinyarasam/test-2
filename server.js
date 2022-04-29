be432df8b0231cc827d332720246d453c9d49d21
kinyarasam <samkinyara2000@gmail.com>
2022-04-29 19:29:57 +0300

    node HTTP module and file system

--- server.js
+++ server.js
@@ -1,7 +1,26 @@
-const express = require("express")
-const app = express()
+// const express = require("express")
+// const app = express()
+const http = require('http')
+const dt = require('./Date')
+const fs = require('fs')
 
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 8080
 
-app.listen(PORT,() =>
-	console.log(`server is running on port ${PORT}`))
+// app.listen(PORT,() =>
+//	console.log(`server is running on port ${PORT}`))
+
+// create a server object
+http.createServer((req, res) => {
+	fs.readFile('testFile.html', (err, data) => {
+	//	res.writeHead(200, {'Content-Type': 'text/html'})
+		res.write(data)
+		// return res.end()
+	// })
+	// res.writeHead(200, {'Content-Type': 'text/html'})
+	res.write('Hello World!')
+	res.write('The current date and time is: ' + dt.myDateTime())
+	res.end()
+	})
+}).listen(PORT, () => 
+	console.log(`Server is running on http://localhost:${PORT}`)
+)
